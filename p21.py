a65d0ebb3da970dbee18c20c8201462accd8664b
PranayP24 <156881909+PranayP24@users.noreply.github.com>
2024-01-18 13:29:43 +0530

    Update p21.py

--- p21.py
+++ p21.py
@@ -1,8 +1,9 @@
 #Check whether two lists are circularly identical
 list1=[1,2,3,4]
 list2=[2,4,3,1]
-index=0
-for x in range(len(list1)):
-    z=0
-    for y in range(len(list2)):
+for each in list1 :
+    if each not in list2:
+        print(Flase)
+else:
+    print(True)
         
