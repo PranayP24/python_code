47a77844e3671e77292ce906db677474ccc41529
PranayP24 <156881909+PranayP24@users.noreply.github.com>
2024-01-18 14:59:15 +0530

    Update p53.py

--- p53.py
+++ p53.py
@@ -1,7 +1,7 @@
 #Print the Fibonacci sequence up to n terms
 fibo_number=int(input('enter the how many terms:'))
 fibonac=[0,1]
-for num in range(2,fibo_number+1):
+for num in range(2,fibo_number):
     fibonac.append(fibonac[num-1]+fibonac[num-2])
 print(fibonac)
 
