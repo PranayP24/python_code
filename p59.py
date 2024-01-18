e1c9ef8e2ca177fd6502884507c57e65556b8484
PranayP24 <156881909+PranayP24@users.noreply.github.com>
2024-01-18 16:23:58 +0530

    Update p59.py

--- p59.py
+++ p59.py
@@ -4,12 +4,12 @@ divisor_value=int(input('enter the divisor value :'))
 quotient=0
 remainder=1
 for num in range(dividend_value):
-    quotient= num // divisor_value
-    remainder=num % divisor_value
+    quotient= dividend_value// divisor_value
+    remainder=dividend_value% divisor_value
 print('integer division:',quotient,'remainder:',remainder)
 num =0
 while num < dividend_value:
-    quotient= num // divisor_value
-    remainder=num % divisor_value
+    quotient= dividend_value// divisor_value
+    remainder=dividend_value% divisor_value
     num+=1
 print('integer division:',quotient,'remainder:',remainder)
